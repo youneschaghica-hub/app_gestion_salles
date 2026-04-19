56087fc4c4f6be1b68c5fd702c0532312ee972a1
youneschaghica-hub <youneschaghica@gmail.com>
2026-04-19 15:04:01 -0400

    ajout salle service

--- services/services_salle.py
+++ services/services_salle.py
@@ -4,3 +4,14 @@ class ServiceSalle:
 
     def __init__(self):
         self.dao = DataSalle()
+from models.salle import Salle
+
+    def ajouter_salle(self, salle: Salle):
+        if not salle.code or not salle.libelle or not salle.type:
+            return False, "données manquantes"
+
+        if salle.capacite < 1:
+            return False, "capacité invalide"
+
+        self.dao.insert_salle(salle)
+        return True, "ajout réussi"
